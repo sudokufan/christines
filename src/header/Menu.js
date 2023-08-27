86eb79c58e27535a9e3197ae8ec959c76fcf6263
Alex Day <aday@hey.com>
2023-08-27 11:33:50 +0100

    created header, removed react logo

---
+++ src/header/Menu.js
@@ -0,0 +1,11 @@
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <header className="header">
+      <span>Christine's Sustainable Supermarket</span>
+    </header>
+  );
+}
+
+export default Menu;
