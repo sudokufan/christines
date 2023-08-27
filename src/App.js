62a38b61a4f796d434e835484bb5fcfb0cbcd5f4
Alex Day <aday@hey.com>
2023-08-27 13:11:51 +0100

    removed react imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./header/Header";
 import TextSlice from "./textSlice/TextSlice";
 
--- src/textSlice/TextSlice.js
+++ src/textSlice/TextSlice.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "./TextSlice.css";
 
 function TextSlice(props) {
