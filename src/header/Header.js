b98ae4db186629fc4e694520a390492efd0613fc
Alex Day <aday@hey.com>
2023-08-27 13:05:29 +0100

    added font to header

--- src/header/Header.js
+++ src/header/Header.js
@@ -5,7 +5,6 @@ function Header() {
   return (
     <header className="header">
       <h1 className="shopName">Christine's Sustainable Supermarket</h1>
-      <span className="address">33 Silver St, Bradford-on-Avon BA15 1JX</span>
     </header>
   );
 }
