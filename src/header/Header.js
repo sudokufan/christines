f4aa7e425bd7109636151a13707c14bdb804c58b
Alex Day <aday@hey.com>
2023-08-27 13:11:16 +0100

    fixed sizing of header

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,7 +4,10 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <h1 className="shopName">Christine's Sustainable Supermarket</h1>
+      <div className="shopName">
+        <h1 className="shopTitle">Christine's</h1>
+        <h2 className="shopSubtitle">Sustainable Supermarket</h2>
+      </div>
     </header>
   );
 }
