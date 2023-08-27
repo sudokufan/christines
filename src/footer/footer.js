b2e69351e87daa17adb9a1bd829c62ae258b4f33
Alex Day <aday@hey.com>
2023-08-27 13:57:13 +0100

    footer opening hours

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -3,7 +3,29 @@ import "./footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footerContent">a</div>
+      <div className="footerContent openingHours">
+        <span className="hoursTitle">Opening Hours</span>
+        <div class="times">
+          <ul class="days">
+            <li className="listItem">Monday</li>
+            <li className="listItem">Tuesday</li>
+            <li className="listItem">Wednesday</li>
+            <li className="listItem">Thursday</li>
+            <li className="listItem">Friday</li>
+            <li className="listItem">Saturday</li>
+            <li className="listItem">Sunday</li>
+          </ul>
+          <ul class="hours">
+            <li className="listItem">9:15 - 5</li>
+            <li className="listItem">9:15 - 5</li>
+            <li className="listItem">9:15 - 5</li>
+            <li className="listItem">9:15 - 6</li>
+            <li className="listItem">9:15 - 5</li>
+            <li className="listItem">9:15 - 5</li>
+            <li className="listItem">Closed</li>
+          </ul>
+        </div>
+      </div>
       <div className="footerContent">b</div>
       <div className="footerContent">c</div>
     </footer>
